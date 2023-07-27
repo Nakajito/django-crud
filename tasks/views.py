f7ee46b8c7202b739764bc12e729ed3a095f05af
Daniel Bonilla <87622543+Nakajito@users.noreply.github.com>
2023-07-27 00:27:44 +0000

    Se modificó el manejo del error integrityerror en la base de datos y se creó la sesión del usuario

--- tasks/views.py
+++ tasks/views.py
@@ -1,25 +1,30 @@
 # importamos render para mostrar una pagina web
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # Utilizar clase para crear formularios
 from django.contrib.auth.forms import UserCreationForm
 # Permite guardar usuarios en la DB
 from django.contrib.auth.models import User
 # Permite utilizar el response
 from django.http import HttpResponse
-
+# Utilizar login (cookie)
+from django.contrib.auth import login
+# Para manejar el error IntegrityError
+from django.db import IntegrityError
 
 
 # definimos la función para mosntrar eh home
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
 
 # Definimos la función para el singup y validar el usuario, contraseña y guardar en la base de datos, primero se deben de realizar las migraciones con <python manage.py migrate>
+
+
 def singup(request):
-    
+
     # Si se accede con el método GET se muestra el formulario
     if request.method == 'GET':
-        return render(request,'singup.html',{
-            'form' : UserCreationForm
+        return render(request, 'singup.html', {
+            'form': UserCreationForm
         })
     # Se se accede con el método POST se validan los datos de usuario, contraseña, y guardar en la base de datos
     else:
@@ -27,14 +32,27 @@ def singup(request):
         if request.POST['password1'] == request.POST['password2']:
             try:
                 # si las contraseñas coinciden se crea el objeto user con el nombre de usuario y contraseña
-                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
                 # Guardamos el usuario en la DB
                 user.save()
+                # Creamos una sesión para el usuario
+                login(request, user)
+                # Redireccionamos a la página tasks
+                return redirect('tasks')
                 # Mostramos el mensaje que se guardó el usuario
-                return HttpResponse('Usuario creado satisfactoriamente')
-            except:
+                # return HttpResponse('Usuario creado satisfactoriamente')
+            except IntegrityError:
                 # En caso de que exista el usuario
-                return HttpResponse('Ya existe el usuario')
+                return render(request, 'singup.html', {
+                    'form': UserCreationForm,
+                    'error': 'El usuario ya esxiste',
+                })
             # Mensaje que indica que las contraseñas son no coinciden
-        return HttpResponse('No coinciden las contraseñas')
-    
+        return render(request, 'singup.html', {
+            'form' : UserCreationForm,
+            'error' : 'Las contraseñas no coinciden'
+        })
+
+def tasks(request):
+    return render(request, 'tasks.html')

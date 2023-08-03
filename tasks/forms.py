3e25fb1d00c563eda7548eaaade2bf9210a01ddd
Daniel Bonilla <87622543+Nakajito@users.noreply.github.com>
2023-08-03 18:58:12 +0000

    se conluyó la funcionalidad de la aplicación

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,7 +1,7 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Task
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = [
@@ -9,3 +9,8 @@ class TaskForm(ModelForm):
                 'description',
                 'important'
             ]
+        widgets = {
+            'title' : forms.TextInput(attrs={'class' : 'form-control', 'placeholder' : 'write a title'}),
+            'description' : forms.Textarea(attrs={'class' : 'form-control'}),
+            'important' : forms.CheckboxInput(attrs={'class' : 'form-check-input'})
+        }
